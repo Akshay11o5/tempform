3aa4bfd80a74f369bc880f868a449fc2bcb58850
Akshay11o5 <thoratakshay8230@gmail.com>
2025-04-06 16:47:02 +0530

    tempform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TempformComponent } from './shared/components/tempform/tempform.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TempformComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/components/tempform/tempform.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-tempform',
+  templateUrl: './tempform.component.html',
+  styleUrls: ['./tempform.component.scss'],
+})
+export class TempformComponent implements OnInit {
+  constructor() {}
+
+  @ViewChild('signUpform') signUpform!: NgForm;
+  ngOnInit(): void {}
+
+  sumbitonclick() {
+    console.log(this.signUpform.value);
+  }
+}
